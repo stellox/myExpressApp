fd7a0719c535c33915b2c98146a1b29981771e4c
pelstix <pelfap2019@gmail.com>
2023-04-20 00:55:37 +0100

    Chnaged to Team-DeVoPS

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Moyosore' });
+  res.render('index', { title: 'Team-DevOps' });
 });
 
 module.exports = router;
