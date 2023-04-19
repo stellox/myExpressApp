06f2d30acd8f2bbbf4a1ab681dea91ca6fce5d80
pelstix <pelfap2019@gmail.com>
2023-04-19 21:18:29 +0100

    Chnaged to Moyo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Pelstix' });
+  res.render('index', { title: 'Moyosore' });
 });
 
 module.exports = router;
