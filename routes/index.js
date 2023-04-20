9925908b4d9812968491c32b0c7d4300cb8d6f9c
pelstix <pelfap2019@gmail.com>
2023-04-20 01:45:14 +0100

    CI/CD to the world

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Team-DevOps' });
+  res.render('index', { title: 'Team-DevOps Loves to Break Stuff and Cook Beans' });
 });
 
 module.exports = router;
